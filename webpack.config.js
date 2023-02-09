6cfb55ddc0c99f9dfcf584dcafed8c0de259d42b
Martin Podloucký <podlouckymartin@gmail.com>
2023-02-09 19:31:10 +0100

    ESLint AirBnB config

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.tsx",
+  mode: 'development',
+  entry: './src/index.tsx',
   output: {
-    path: __dirname + "/dist/",
+    path: `${__dirname}/dist/`,
     filename: 'bundle-[contenthash:6].js',
   },
   module: {
@@ -14,26 +14,26 @@ module.exports = {
         test: /\.(ts|tsx)$/,
         exclude: /node_modules/,
         resolve: {
-          extensions: [".ts", ".tsx", ".js", ".json"],
+          extensions: ['.ts', '.tsx', '.js', '.json'],
         },
-        use: "ts-loader",
+        use: 'ts-loader',
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
       {
         test: /\.jpe?g|png|svg|webp$/,
         type: 'asset/resource',
         generator: {
-          filename: 'img/[name]-[contenthash:6][ext]'
-        }
+          filename: 'img/[name]-[contenthash:6][ext]',
+        },
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "index.html",
+      template: 'index.html',
     }),
     new MiniCssExtractPlugin(),
   ],
