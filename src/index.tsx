e1e461ce03ee94b4163663eaafc5977298e2254b
jitka.maresova <jitka.maresova@dnai.ai>
2023-02-10 08:26:34 +0100

    added prettier, router-dom, HomePage

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client';
+import App from './App';
 
 const container = document.getElementById('app-root')!;
 const root = createRoot(container);
-root.render(<h1>Hello React!</h1>);
+root.render(<App />);
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import classes from './HomePage.module.css';
+
+const HomePage = (): JSX.Element => {
+  return <div className={classes.home}>HomePage</div>;
+};
+
+export default HomePage;
