d3795541a731f549b57510df1ca33c2530681ba6
jitka.maresova <jitka.maresova@dnai.ai>
2023-02-15 21:28:54 +0100

    added home page card, css reset

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import classes from './HomePage.module.css';
+import HomePageCard from '../components/home/HomePageCard';
 
 const HomePage = (): JSX.Element => {
-  return <div className={classes.home}>HomePage</div>;
+  return (
+    <div className={classes.home}>
+      <HomePageCard />
+    </div>
+  );
 };
 
 export default HomePage;
