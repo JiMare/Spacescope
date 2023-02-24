4aac4b8e7491ce90a9196e0974ee4b704b4fcec9
Martin Podloucký <podlouckymartin@gmail.com>
2023-02-24 19:04:53 +0100

    SASS config

--- webpack.config.js
+++ webpack.config.js
@@ -20,8 +20,8 @@ module.exports = {
         use: "ts-loader",
       },
       {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        test: /\.s?css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
       {
         test: /\.jpe?g|png|svg|webp$/,
